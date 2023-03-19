c2da4a821aaeadccf139dc04c3612025a12afc2b
houjianhua1 <119669289+houjianhua1@users.noreply.github.com>
2023-03-19 11:13:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f1a63c7b5bba57bee5647e60d4b06c63',
 
-  PROVINCE: '江苏省',
-  CITY: '连云港市',
+  PROVINCE: '江苏',
+  CITY: '连云港',
 
   USERS: [
     {
